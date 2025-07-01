108cf3553726609ae277167767f6bcc7449f6c29
Abhishekkochar <abhishekkochar2@gmail.com>
2025-07-01 19:39:24 +1000

    Remove submodules, switching to forge-managed dependencies

---
+++ solidity/script/SwapV4.s.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {console} from "forge-std/Script.sol";
+import {UniversalRouter} from "@uniswap/universal-router/contracts/UniversalRouter.sol";
+import {Commands} from "@uniswap/universal-router/contracts/libraries/Commands.sol";
+import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
+import {IV4Router} from "@uniswap/v4-periphery/src/interfaces/IV4Router.sol";
+import {Actions} from "@uniswap/v4-periphery/src/libraries/Actions.sol";
+import {IPermit2} from "@uniswap/permit2/src/interfaces/IPermit2.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {StateLibrary} from "@uniswap/v4-core/src/libraries/StateLibrary.sol";
+import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
+
+import {SepoliaConstants} from "./constants.sol";
+import {ScriptHelper} from "./deployments/ScriptHelper.s.sol";
+
+contract SwapV4 is ScriptHelper {
+    using StateLibrary for IPoolManager;
+
+    UniversalRouter router;
+    IPoolManager poolManager;
+    IPermit2 permit2;
+
+    // Proxy pool tokens (underlying tokens)
+    address usdcToken;
+    address usdtToken;
+
+    function run() external {
+        router = UniversalRouter(payable(SepoliaConstants.UNIVERSAL_ROUTER));
+        poolManager = IPoolManager(SepoliaConstants.POOL_MANAGER);
+        permit2 = IPermit2(SepoliaConstants.PERMIT2);
+
+        // Proxy pool tokens (underlying tokens)
+        usdcToken = readAddress("usdcToken");
+        usdtToken = readAddress("usdtToken");
+    }
+
+    function approveTokenWithPermit2(
+        address token,
+        uint160 amount,
+        uint48 expiration
+    ) external {
+        IERC20(token).approve(address(permit2), type(uint256).max);
+        permit2.approve(token, address(router), amount, expiration);
+    }
+
+    function swapExactInputSingle(
+        PoolKey calldata key, // PoolKey struct that identifies the v4 pool
+        uint128 amountIn, // Exact amount of tokens to swap
+        uint128 minAmountOut, // Minimum amount of output tokens expected
+        uint256 deadline // Timestamp after which the transaction will revert
+    ) external returns (uint256 amountOut) {
+        bytes memory commands = abi.encodePacked(uint8(Commands.V4_SWAP));
+
+        // Encode V4Router actions
+        bytes memory actions = abi.encodePacked(
+            uint8(Actions.SWAP_EXACT_IN_SINGLE),
+            uint8(Actions.SETTLE_ALL),
+            uint8(Actions.TAKE_ALL)
+        );
+    }
+}
--- solidity/script/constants.sol
+++ solidity/script/constants.sol
@@ -5,7 +5,11 @@ pragma solidity ^0.8.0;
 library SepoliaConstants {
     // To deploy uniswap pool
     address constant POOL_MANAGER = 0xFB3e0C6F74eB1a21CC1Da29aeC80D2Dfe6C9a317;
-    address constant POSITION_MANAGER = 0xAc631556d3d4019C95769033B5E719dD77124BAc;
-    address constant DEPLOYER_CREATE2 = 0x4e59b44847b379578588920cA78FbF26c0B4956C;
+    address constant POSITION_MANAGER =
+        0xAc631556d3d4019C95769033B5E719dD77124BAc;
+    address constant DEPLOYER_CREATE2 =
+        0x4e59b44847b379578588920cA78FbF26c0B4956C;
     address constant PERMIT2 = 0x000000000022D473030F116dDEE9F6B43aC78BA3;
+    address constant UNIVERSAL_ROUTER =
+        0xeFd1D4bD4cf1e86Da286BB4CB1B8BcED9C10BA47;
 }
