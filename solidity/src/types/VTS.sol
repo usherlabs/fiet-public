ec422df34dd5263eb35828afdb256e3cdb466edc
Ryan Soury <rsouryis@hotmail.com>
2025-09-27 11:55:22 +1000

    comment on VTS timewindow

--- solidity/src/types/VTS.sol
+++ solidity/src/types/VTS.sol
@@ -15,7 +15,10 @@ struct MarketVTSConfiguration {
     TokenConfiguration token0;
     // Token configuration for token1
     TokenConfiguration token1;
-    // Time t
+    // Time t in seconds.
+    // - Governs how long market demand is tracked for. Predicated on the question: What does demand mean for this token?
+    // - Higher tracked market demand, means requiring more liquidity settled.
+    // - Can be 5 minutes (high volume + rapid settlements) -> 48 hours (low volume + slow settlements) depending on the market.
     uint256 timeWindow;
     // oracle address for this market, can be address(0) to use the default oracle
     address oracleFactory;
