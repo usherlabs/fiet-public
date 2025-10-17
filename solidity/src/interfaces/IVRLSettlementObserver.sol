55fbf7a594548f96a964ad4d19075bebb23ea9d5
xander <professor.alex88@gmail.com>
2025-10-17 06:01:04 +0100

    forge fmt

--- solidity/src/interfaces/IVRLSettlementObserver.sol
+++ solidity/src/interfaces/IVRLSettlementObserver.sol
@@ -2,7 +2,5 @@
 pragma solidity ^0.8.0;
 
 interface IVRLSettlementObserver {
-    function verifySettlementProof(uint256 verifierIndex, bytes memory settlementProof)
-        external
-        returns (uint256);
+    function verifySettlementProof(uint256 verifierIndex, bytes memory settlementProof) external returns (uint256);
 }
