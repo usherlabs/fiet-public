f926373692619f60b1951f7fd0aa133ce1ab56b8
Ryan Soury <rsouryis@hotmail.com>
2025-11-01 14:05:49 +1100

    tests all passing

--- solidity/test/MarketMaker.t.sol
+++ solidity/test/MarketMaker.t.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 import {MarketMaker} from "../src/libraries/MarketMaker.sol";
-import {console} from "forge-std/console.sol";
 import {ICSpokeVerifier} from "../src/modules/ICSpokeVerifier.sol";
 import {Test} from "forge-std/Test.sol";
 import {MarketMakerTestBase} from "./modules/MMTestBase.sol";
@@ -23,10 +22,7 @@ contract MarketMakerTest is MarketMakerTestBase {
         bytes32 leafHash = liquiditySignal.mmState.toLeafHash();
 
         // Verify the hash is equal to the expected value
-        // to verify hashing algorithm has not been changed
-        // NOTE: This hash needs to be recalculated after switching from sha256 to keccak256
-        // Run the test with -vvv to see the actual hash output, then update this value
-        bytes32 expected_leaf_hash = bytes32(0x8b16fa0ca40552353a38f252dd251d4ef01166924606707a802441e94fb01cc2);
+        bytes32 expected_leaf_hash = keccak256(abi.encode(liquiditySignal.mmState));
         assertEq(leafHash, expected_leaf_hash);
     }
 
