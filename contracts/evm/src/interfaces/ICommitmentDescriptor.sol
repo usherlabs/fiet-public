b43519d94b789b85d2b283e173cbab95790a3af1
Ryan Soury <rsouryis@hotmail.com>
2025-12-10 03:54:53 +1100

    refine mmpm commit descriptor

--- contracts/evm/src/interfaces/ICommitmentDescriptor.sol
+++ contracts/evm/src/interfaces/ICommitmentDescriptor.sol
@@ -8,10 +8,9 @@ pragma solidity ^0.8.0;
 interface ICommitmentDescriptor {
     /**
      * @notice Generates a token URI for a commitment NFT
-     * @param manager The address of the MMPositionManager contract
      * @param tokenId The token ID of the commitment NFT
      * @return The token URI as a data URI containing JSON metadata
      */
-    function tokenURI(address manager, uint256 tokenId) external view returns (string memory);
+    function tokenURI(uint256 tokenId) external view returns (string memory);
 }
 
